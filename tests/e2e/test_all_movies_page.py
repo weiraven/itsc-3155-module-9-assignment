4e1c57f573e8112097701e93ccbdfd642677e721
brandonhach <specialcambo@gmail.com>
2023-10-20 18:39:46 -0400

    Finish writing unit test and e2e test for list_all_movies (ticket 1)

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -2,10 +2,12 @@
 from app import app
 import pytest
 
+
 @pytest.fixture()
 def test_app():
     return app.test_client()
 
+
 def test_endpoint_movies(test_app):
-    response = test_app.get('/movies')
+    response = test_app.get("/movies")
     assert response.status_code == 200
--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -2,6 +2,7 @@
 import pytest
 from flask.testing import FlaskClient
 from app import app
+from src.repositories.movie_repository import get_movie_repository
 
 
 @pytest.fixture()
@@ -9,11 +10,17 @@ def test_app():
     return app.test_client()
 
 
-def test_get_all_movies(test_app: FlaskClient) -> None:
+def test_no_movies_in_view(test_app: FlaskClient) -> None:
     response = test_app.get("/movies")
+    data = response.data.decode()
+    assert "No movies have been registered yet.</h2>" in data
     assert response.status_code == 200
 
 
-def test_get_movie_not_found(test_app: FlaskClient) -> None:
-    response = test_app.get("/movies/9999")
-    assert response.status_code == 404
+def test_movies_in_view(test_app: FlaskClient) -> None:
+    movie_repository = get_movie_repository()
+    movie_repository.create_movie("dookie", "cheesee", 10)
+    response = test_app.get("/movies")
+    data = response.data.decode()
+    assert "dookie</td>" in data
+    assert response.status_code == 200
