b372ab5a0de4be9f4aee466b3eb19d3cedaca231
brandonhach <specialcambo@gmail.com>
2023-10-20 20:31:21 -0400

    Fixed unit test to include pytest fixture

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,8 +1,14 @@
 # TODO: Feature 1
-from app import list_all_movies
 import pytest
+from flask.testing import FlaskClient
+from app import app
 
 
-def test_endpoint_movies(test_app):
+@pytest.fixture()
+def test_app() -> FlaskClient:
+    return app.test_client()
+
+
+def test_endpoint_movies(test_app: FlaskClient) -> None:
     response = test_app.get("/movies")
     assert response.status_code == 200
