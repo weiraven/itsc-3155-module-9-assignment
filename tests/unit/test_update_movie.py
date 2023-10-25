1c201a8186807099514180777fb612b4f4a7da8d
Damon Nitsavong <dnitsavo@uncc.edu>
2023-10-24 22:58:13 -0400

    Added unit testing

--- tests/unit/test_update_movie.py
+++ tests/unit/test_update_movie.py
@@ -1,1 +1,19 @@
-# TODO: Feature 5
+from src.models.movie import Movie
+from src.repositories.movie_repository import get_movie_repository
+
+
+def test_update_movie():
+
+    movie = get_movie_repository()
+    movie.clear_db
+
+    movie.create_movie('Star Wars', 'George Lucas', 5)
+    movies = list(movie.get_all_movies().values())
+    id = int(movies[0].movie_id)
+
+    get_movie_repository().update_movie(id, 'The Goonies', 'Richard Donner', 4)
+    assert movies[0].title == 'The Goonies'
+    assert movies[0].director == 'Richard Donner'
+    assert movies[0].rating == 4
+
+    
