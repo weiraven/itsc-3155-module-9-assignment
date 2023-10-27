7f6b6debf3a437aaeca20f3ada37e9a114262d97
Rachel <rachele912@gmail.com>
2023-10-27 11:51:20 -0400

    Completed Search Functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -33,8 +33,13 @@ def create_movie():
 
 @app.get('/movies/search')
 def search_movies():
-    # TODO: Feature 3
-    return render_template('search_movies.html', search_active=True)
+    title = request.args.get('search')
+    movie = movie_repository.get_movie_by_title(title)
+    if movie:
+        return render_template('search_movies.html', search_active=True, movie=movie)
+    else:
+        return render_template('search_movies.html', search_active=True, error="Movie Not Found")
+    
 
 
 @app.get('/movies/<int:movie_id>')
